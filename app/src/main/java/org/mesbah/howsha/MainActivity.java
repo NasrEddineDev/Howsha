dfa777e1afe37c80067d93fd187e947a6a5eaae6
nasreddine <nasreddine.guelfout@gmail.com>
2022-06-18 23:21:36 +0100

    second commit

--- app/src/main/java/org/mesbah/howsha/MainActivity.java
+++ app/src/main/java/org/mesbah/howsha/MainActivity.java
@@ -1,14 +1,23 @@
 package org.mesbah.howsha;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.PhoneNumberUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
 import java.util.concurrent.TimeUnit;
@@ -16,8 +25,8 @@ import java.util.concurrent.TimeUnit;
 public class MainActivity extends AppCompatActivity {
 
     private EditText etPhoneNumber, etVerificationCode;
-    private Button bVerify;
-
+    private Button bVerify, bSend;
+    private String code, verificationId;
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks callbacks;
 
     @Override
@@ -29,13 +38,64 @@ public class MainActivity extends AppCompatActivity {
         etPhoneNumber = findViewById(R.id.etPhoneNumber);
         etVerificationCode = findViewById(R.id.etVerificationCode);
         bVerify = findViewById(R.id.bVerify);
+        bSend = findViewById(R.id.bSend);
 
-        bVerify.setOnClickListener(new View.OnClickListener() {
+        bSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startPhoneNumberVerification();
             }
         });
+
+        bVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verifyPhoneNumberWithCode();
+            }
+        });
+
+        callbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(verificationId, forceResendingToken);
+                verificationId = verificationId;
+            }
+        };
+    }
+
+    private void verifyPhoneNumberWithCode() {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, etVerificationCode.getText().toString());
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
+        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                    userIsLoggedIn();
+            }
+        });
+    }
+
+    private void userIsLoggedIn() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null){
+            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+            finish();
+            return;
+        }
     }
 
     private void startPhoneNumberVerification() {
